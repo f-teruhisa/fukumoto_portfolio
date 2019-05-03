56471be2239a778fd96b5eeb26ac24f8a26d01cc
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-03 23:43:03 +0900

    change skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -2,28 +2,45 @@ import React from 'react';
 
 const Skill = () => (
   <React.Fragment>
-    <h2>Skill</h2>
-    <ul>
-      <li>Ruby</li>
-      <li>Ruby on Rails</li>
-      <li>Python</li>
-      <li>Flask</li>
-      <li>Machine Learning</li>
-      <li>ES6</li>
-      <li>React.js</li>
-      <li>Vue.js</li>
-      <li>Node.js</li>
-      <li>Java</li>
-      <li>Android App</li>
-      <li>HTML5</li>
-      <li>CSS3</li>
-      <li>AWS</li>
-      <li>Firebase</li>
-      <li>Heroku</li>
-      <li>Linux</li>
-      <li>Git</li>
-      <li>Circle CI</li>
-    </ul>
+    <h2>Development</h2>
+      <ul>
+        <li>Ruby(rspec)</li>
+        <li>Rails</li>
+        <li>Python(pytest)</li>
+        <li>HTML5(slim, erb)</li>
+        <li>CSS3(SCSS, SASS)</li>
+        <li>Git</li>
+        <li>Flask [Private]</li>
+        <li>JavaScript(ES6)</li>
+        <li>React.js [Private]</li>
+        <li>Node.js [Private]</li>
+        <li>AWS [Private]</li>
+        <li>Firebase [Private]</li>
+      </ul>
+    <h2>Management</h2>
+      <ul>
+        <li>Product Management</li>
+        <li>1on1</li>
+        <li>OKR</li>
+        <li>Engineer Evaluation</li>
+        <li>Organization Planning</li>
+        <li>Project Management</li>
+        <li>People & Talent Management</li>
+      </ul>
+    <h2>Presentation</h2>
+      <ul>
+        <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/07-biao-can-dao-dot-rb-deng-tan-zi-liao">Omotesando.rb『技術を使わない問題解決』</a></li>
+        <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/tsurami-on-rails-at-startup">Gotanda.rb『スタートアップにおけるRails開発のツラみ』</a></li>
+        <li>Product：<a href="https://speakerdeck.com/teruhisafukumoto/messenger-apidegan-zirukomiyunikesiyonfalsewei-lai">Facebook『Messenger APIで感じるコミュニケーションの未来』</a></li>
+        <li>Data：<a href="https://speakerdeck.com/teruhisafukumoto/data-driven-communication-76c4ca0a-d387-4cb9-91fe-dcde2e77e50c">VUILT『Goalから逆算するコミュニケーションデータ戦略』</a></li>
+      </ul>
+    <h2>Article</h2>
+      <ul>
+        <li>PM：<a href="https://medium.com/@teruhisafukumoto/%E3%83%97%E3%83%AD%E3%83%80%E3%82%AF%E3%83%88%E3%83%9E%E3%83%8D%E3%83%BC%E3%82%B8%E3%83%A3%E3%83%BC%E3%81%AF-%E8%87%AA%E5%88%86%E9%81%94%E3%81%8C-%E9%A0%AD%E3%81%8C%E3%81%84%E3%81%84-%E3%81%93%E3%81%A8%E3%82%92%E7%90%86%E8%A7%A3%E3%81%97%E3%81%AA%E3%81%91%E3%82%8C%E3%81%B0%E3%81%AA%E3%82%89%E3%81%AA%E3%81%84-97eaa0d1dc79">『プロダクトマネージャーは、自分達が「頭がいい」ことを理解しなければならない』</a></li>
+        <li>PM：<a href="https://qiita.com/Terryy/items/b3a26517b584cdbec70d">『小さな会社のPMになったあなたへ』</a></li>
+        <li>PM：<a href="https://medium.com/@teruhisafukumoto/evernote%E3%81%AF%E3%81%AA%E3%81%9C%E3%83%80%E3%83%A1%E3%81%AA%E3%81%AE%E3%81%8B-78b5034a8c5a">『Evernoteはなぜダメなのか』</a></li>
+        <li>PM：<a href="https://tech.zeals.co.jp/entry/2019/02/12/125841">『元トレタCTOの増井さんと語る、ZEALSのチャットボットプロダクト『fanp』のすべて（前編）』</a></li>
+      </ul>
   </React.Fragment>
 );
 
