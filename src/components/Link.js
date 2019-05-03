efb93b9c1826e04e910b2f7997ca805e55e7fa97
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-03 22:34:43 +0900

    edit links/

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 const Links = () => (
   <React.Fragment>
-    <h2>Link</h2>
+    <h2>Links</h2>
     <ul>
-      <li>Qiita</li>
-      <a href="https://qiita.com/Sinhalite">@Sinhalite</a>
-      <li>Twitter</li>
-      <a href="https://twitter.com/Sinhalite2">@Sinhalite2</a>
+      <li>Twitter：<a href="https://twitter.com/terry_i_">@terry_i_</a></li>
+      <li>Blog：<a href="https://medium.com/@teruhisafukumoto">Medium(@teruhisafukumoto)</a></li>
+      <li>Github：<a href="https://github.com/f-teruhisa">f-teruhisa</a></li>
+      <li>Qiita：<a href="https://qiita.com/Terryy">Terryy</a></li>
+      <li>Facebook：<a href="https://www.facebook.com/teruhisa.fukumoto">teruhisa.fukumoto</a></li>
+      <li>SpeakerDeck：<a href="https://speakerdeck.com/teruhisafukumoto">teruhisafukumoto</a></li>
+      <li>bookmeter：<a href="https://bookmeter.com/users/629901">Teruhisa Fukumoto</a></li>
+      <li>filmarks：<a href="https://filmarks.com/users/teruid">TeruFukumoto</a></li>
+      <li>NewsPicks：<a href="https://newspicks.com/user/613311">福本 晃之</a></li>
+      <li>Wantedly：<a href="https://www.wantedly.com/users/47151584">福本 晃之</a></li>
+      <li>Linkedin：<a href="https://www.linkedin.com/in/teruhisafukumoto">Teruhisa Fukumoto</a></li>
     </ul>
   </React.Fragment>
 );
