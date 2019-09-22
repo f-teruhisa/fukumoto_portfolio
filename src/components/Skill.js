7c957b5723d204952ed71e4e66d6599762d68cac
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-09-22 20:01:36 +0900

    update skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -4,15 +4,16 @@ const Skill = () => (
   <React.Fragment>
     <h2>Development</h2>
       <ul>
-        <li>Ruby(rspec)</li>
+        <li>Ruby</li>
         <li>Rails</li>
-        <li>Python(pytest)</li>
-        <li>HTML5(slim, erb)</li>
-        <li>CSS3(SCSS, SASS)</li>
+        <li>Python</li>
+        <li>HTML</li>
+        <li>CSS3</li>
         <li>Git</li>
         <li>Flask [Private]</li>
         <li>JavaScript(ES6)</li>
-        <li>React.js [Private]</li>
+        <li>Vue.js</li>
+        <li>React.js[Private]</li>
         <li>Node.js [Private]</li>
         <li>AWS [Private]</li>
         <li>Firebase [Private]</li>
@@ -33,12 +34,12 @@ const Skill = () => (
         <li>Project Management</li>
       </ul>
     <h2>Presentation</h2>
-      <ul>
-        <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/07-biao-can-dao-dot-rb-deng-tan-zi-liao">Omotesando.rb『技術を使わない問題解決』</a></li>
-        <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/tsurami-on-rails-at-startup">Gotanda.rb『スタートアップにおけるRails開発のツラみ』</a></li>
-        <li>Product：<a href="https://speakerdeck.com/teruhisafukumoto/messenger-apidegan-zirukomiyunikesiyonfalsewei-lai">Facebook『Messenger APIで感じるコミュニケーションの未来』</a></li>
-        <li>Data：<a href="https://speakerdeck.com/teruhisafukumoto/data-driven-communication-76c4ca0a-d387-4cb9-91fe-dcde2e77e50c">VUILT『Goalから逆算するコミュニケーションデータ戦略』</a></li>
-      </ul>
+    <ul>
+      <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/07-biao-can-dao-dot-rb-deng-tan-zi-liao">Omotesando.rb『技術を使わない問題解決』</a></li>
+      <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/tsurami-on-rails-at-startup">Gotanda.rb『スタートアップにおけるRails開発のツラみ』</a></li>
+      <li> Career：<a href="https://speakerdeck.com/teruhisafukumoto/what-i-thought-after-experiencing-with-pm-em-devrel-recruitment">Enginner Career Night『PMとEMとDevRelと採用を経験して見えた話』</a></li>
+      <li> UX： <a href="https://speakerdeck.com/teruhisafukumoto/push-and-delivery-ux-at-chatbot">UX MILK Fest 2019『チャットボットで考えるプッシュ＆デリバリーUX』</a></li>
+    </ul>
     <h2>Article</h2>
       <ul>
         <li>PM：<a href="https://medium.com/@teruhisafukumoto/%E3%83%97%E3%83%AD%E3%83%80%E3%82%AF%E3%83%88%E3%83%9E%E3%83%8D%E3%83%BC%E3%82%B8%E3%83%A3%E3%83%BC%E3%81%AF-%E8%87%AA%E5%88%86%E9%81%94%E3%81%8C-%E9%A0%AD%E3%81%8C%E3%81%84%E3%81%84-%E3%81%93%E3%81%A8%E3%82%92%E7%90%86%E8%A7%A3%E3%81%97%E3%81%AA%E3%81%91%E3%82%8C%E3%81%B0%E3%81%AA%E3%82%89%E3%81%AA%E3%81%84-97eaa0d1dc79">『プロダクトマネージャーは、自分達が「頭がいい」ことを理解しなければならない』</a></li>
