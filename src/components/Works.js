79dbe5ba91da3859d1d8a69812493b8d11b06019
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-12-28 22:20:09 +0900

    update skills and outputs

--- src/components/Works.js
+++ src/components/Works.js
@@ -3,9 +3,22 @@ import Application from '../Works/Application';
 
 const Works = () => (
   <div id="works">
-    <h2>Works</h2>
-    <p>プライベートでの開発アウトプット一覧</p>
+    <h2>Service</h2>
     <Application />
+    <h2>Article</h2>
+      <ul>
+        <li>PM：<a href="https://medium.com/@teruhisafukumoto/%E3%83%97%E3%83%AD%E3%83%80%E3%82%AF%E3%83%88%E3%83%9E%E3%83%8D%E3%83%BC%E3%82%B8%E3%83%A3%E3%83%BC%E3%81%AF-%E8%87%AA%E5%88%86%E9%81%94%E3%81%8C-%E9%A0%AD%E3%81%8C%E3%81%84%E3%81%84-%E3%81%93%E3%81%A8%E3%82%92%E7%90%86%E8%A7%A3%E3%81%97%E3%81%AA%E3%81%91%E3%82%8C%E3%81%B0%E3%81%AA%E3%82%89%E3%81%AA%E3%81%84-97eaa0d1dc79">『プロダクトマネージャーは、自分達が「頭がいい」ことを理解しなければならない』</a></li>
+        <li>Developer Relation：<a href="https://medium.com/@teruhisafukumoto/%E3%83%86%E3%83%83%E3%82%AF%E3%83%96%E3%83%AD%E3%82%B0-%E7%99%BB%E5%A3%87-%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88-%E4%BC%9A%E7%A4%BE%E3%81%A7%E6%8A%80%E8%A1%93%E5%BA%83%E5%A0%B1%E3%82%92%E7%B6%99%E7%B6%9A%E3%81%97%E3%81%A6%E8%A1%8C%E3%81%86%E3%81%9F%E3%82%81%E3%81%AE%E3%82%AC%E3%82%A4%E3%83%89%E3%83%A9%E3%82%A4%E3%83%B3-4c3b40039399">『テックブログ、登壇、イベント…企業が技術広報を継続して行うためのガイドライン』</a></li>
+        <li>PM：<a href="https://qiita.com/Terryy/items/b3a26517b584cdbec70d">『小さな会社のPMになったあなたへ』</a></li>
+        <li>Administration：<a href="https://getnews.jp/archives/2158006">『人類史上最もHardな意思決定『キューバ危機』から学ぶ意思決定のすすめ方(ガジェット通信)』</a></li>
+      </ul>
+    <h2>Presentation</h2>
+    <ul>
+      <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/07-biao-can-dao-dot-rb-deng-tan-zi-liao">Omotesando.rb『技術を使わない問題解決』</a></li>
+      <li>Ruby：<a href="https://speakerdeck.com/teruhisafukumoto/awareness-of-oss-contribution-by-sending-pr-to-rails">平成RubyKaigi『RailsにPRを送って発見したOSS貢献への気づき』</a></li>
+      <li>Engineering Management：<a href="https://speakerdeck.com/teruhisafukumoto/what-i-thought-after-experiencing-with-pm-em-devrel-recruitment">Enginner Career Night『PMとEMとDevRelと採用を経験して見えた話』</a></li>
+      <li>UX Design： <a href="https://speakerdeck.com/teruhisafukumoto/push-and-delivery-ux-at-chatbot">UX MILK Fest 2019『チャットボットで考えるプッシュ＆デリバリーUX』</a></li>
+    </ul>
   </div>
 );
 
