13aae938cb4357901fa48950409a9cc259ffb663
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-02-08 16:08:46 +0900

    add firebase.json

---
+++ firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMg7BGy1b92aL-XTROTM34LduL3GH4Kcw",
+  authDomain: "fukumoto-portfolio.firebaseapp.com",
+  databaseURL: "https://fukumoto-portfolio.firebaseio.com",
+  projectId: "fukumoto-portfolio",
+  storageBucket: "fukumoto-portfolio.appspot.com",
+  messagingSenderId: "542050596861",
+  appId: "1:542050596861:web:963e602f4ee6d9c8e1cb94"
+};
+
+firebase.initializeApp(config);
