587bc639b2380e53af0da4eb94c3039825e5d56c
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-04-04 19:20:17 +0900

    add form with material ui

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
 
-const Contact = () => (
-  <React.Fragment>
-    <h2>Contact</h2>
-    <a href="https://tayori.com/form/45957d1747b8a2c3b2d2bb43ed76437012925f19" target="_brank" rel="noopener noreferrer">お問い合わせはこちら</a>
-  </React.Fragment>
-);
+const firebase = require("firebase");
+require("firebase/functions");
 
-export default Contact;
+
+class ContactForm extends Component {
+    constructor(){
+        super()
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(e){
+        e.preventDefault()
+        let data = {}
+        data.name = e.target.name.value
+        data.email = e.target.email.value
+        data.content = e.target.content.value
+        let sendMail = firebase.functions().httpsCallable('sendMail');
+        sendMail(data)
+        e.target.name.value = ""
+        e.target.email.value = ""
+        e.target.content.value = ""
+        e.target.value = ""
+    }
+
+    render() {
+        const textFieldStyle = {
+            display: "flex",
+            width: "100%",
+        }
+
+        const contactForm = {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "left",
+            margin: "20px",
+        }
+
+
+        return (
+            <React.Fragment>
+            <h2>Contact</h2>
+                <div style={contactForm}>
+                    <form onSubmit={this.onSubmit}>
+                        <TextField name="name" label="Name" type="text" required style={textFieldStyle}  />
+                        <TextField name="email" label="Email" type="mail" required style={textFieldStyle}   />
+                        <TextField
+                            required
+                            name="content"
+                            label="Contents..."
+                            multiline
+                            rows="8"
+                            margin="normal"
+                            variant="outlined"
+                            style={textFieldStyle}
+                        />
+                        <Button variant="contained" color="primary" type="submit" style={textFieldStyle} >
+                            Submit
+                        </Button>
+                    </form>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ContactForm
