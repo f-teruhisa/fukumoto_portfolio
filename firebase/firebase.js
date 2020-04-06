ead6a544c2e95487e6bf5a4e2cf88bec63f4b28a
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2020-04-06 23:19:43 +0900

    change font and fix contact form

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import 'firebase/auth';
 
-const firebaseConfig = {
+const config = {
   apiKey: "AIzaSyDMg7BGy1b92aL-XTROTM34LduL3GH4Kcw",
   authDomain: "fukumoto-portfolio.firebaseapp.com",
   databaseURL: "https://fukumoto-portfolio.firebaseio.com",
@@ -9,6 +9,6 @@ const firebaseConfig = {
   storageBucket: "fukumoto-portfolio.appspot.com",
   messagingSenderId: "542050596861",
   appId: "1:542050596861:web:963e602f4ee6d9c8e1cb94"
-};
+}
 
 firebase.initializeApp(config);
